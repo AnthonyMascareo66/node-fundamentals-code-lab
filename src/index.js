455edbbbed13724c15e48af35435766a7a2825af
AnthonyMascareo66 <demonsoul66677@gmail.com>
2023-11-16 21:45:36 -0600

    agregado index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// index.js
+const { showResult } = require('./add');
+
+// Llamar a la función showResult con dos números como parámetros
+const resultadoColoreado = showResult(2, 3);
+
+// Mostrar el resultado coloreado en la consola
+console.log(resultadoColoreado);
